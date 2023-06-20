8414432b7f6da7ce47bb811342e70592a1028bce
NikyDS <dossantosnikoly01@gmail.com>
2023-06-20 12:40:10 -0400

    Changes for day 2 before lunch

---
+++ BasicConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+
+Console.WriteLine("Hello from .Net Top Level Statements");
+Console.WriteLine("Another Line");
+
+List<string> friends = new List<string>();
+friends.Add("David");
+
+Console.WriteLine($"You have {friends.Count} friends");
